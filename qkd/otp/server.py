c1c4e71af1cf66cb609f38d94435c8ecc3cd84e4
Edoardo <edofru01@protonmail.com>
2025-12-31 18:30:25 +0100

    added a test line to the file

--- qkd/otp/server.py
+++ qkd/otp/server.py
@@ -21,6 +21,8 @@ def handle_client(client_socket, data):
 def main():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind(('10.40.7.121', 8080))
+    # server_socket.bind(('127.0.0.1', 8080))
+
     server_socket.listen(1)
 
     print("Server is listening on port 8080...")
